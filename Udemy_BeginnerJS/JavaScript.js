17279d642a5786d37760cf1c1f853f128dd18ed7
Amy Thompson <amythompson9@gmail.com>
2018-05-29 16:00:51 -0400

    JS alert/prompt/console.log

--- Udemy_BeginnerJS/JavaScript.js
+++ Udemy_BeginnerJS/JavaScript.js
@@ -1,1 +1,6 @@
-﻿
+﻿//alert("hello from the JS file")
+//code will run when the page refreshes
+
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
